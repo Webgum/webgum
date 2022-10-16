8188e8b57ba4cbd234bbd7d475acc20a7abd5126
cuddleofdeath <cuddleofdeath@gmail.com>
2022-10-16 17:35:08 -0500

    Updated spelling for Mathematics

--- pages/index.tsx
+++ pages/index.tsx
@@ -83,7 +83,7 @@ const Home: NextPage = () => {
         <Grid xs={12} sm={4}>
           <GumCard
             label='New Product'
-            title='Mathmatics for Cats'
+            title='Mathematics for Cats'
             imageUrl='https://images.unsplash.com/photo-1491485880348-85d48a9e5312?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
             price={1}
             rating={5}

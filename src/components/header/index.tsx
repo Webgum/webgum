fdc7827a97d7f15d602ce8c319b4b53759818b03
cuddleofdeath <cuddleofdeath@gmail.com>
2022-11-06 19:52:02 -0600

    Added react-icons and replaced old header icons

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,23 +1,31 @@
 import styles from './header.module.scss';
 import SearchIcon from '../../assets/search-interface-symbol.png';
 import AccountIcon from '../../assets/user.png';
-import ShoppingCartIcon from '../../assets/shopping-cart.png';
 import Image from 'next/image';
+import {
+  AiOutlineSearch,
+  AiOutlineShoppingCart,
+  AiOutlineUser,
+} from 'react-icons/ai';
 
 const Header = () => {
   return (
     <div className={styles.container}>
       <div className={styles.logo}>webgum</div>
+
       <div className={styles.menus}>
         <button className={styles.sellBtn}>Start Selling</button>
         <div className={styles.menuWrapper}>
-          <Image src={AccountIcon} objectFit='cover' layout='fill' />
+          {/* <Image src={AccountIcon} objectFit='cover' layout='fill' /> */}
+          <AiOutlineUser size='35px' />
         </div>
         <div className={styles.menuWrapper}>
-          <Image src={ShoppingCartIcon} objectFit='cover' layout='fill' />
+          {/* <Image src={ShoppingCartIcon} objectFit='cover' layout='fill' /> */}
+          <AiOutlineShoppingCart size='35px' />
         </div>
         <div className={styles.menuWrapper}>
-          <Image src={SearchIcon} objectFit='cover' layout='fill' />
+          {/* <Image src={SearchIcon} objectFit='cover' layout='fill' /> */}
+          <AiOutlineSearch size='35px' />
         </div>
       </div>
     </div>

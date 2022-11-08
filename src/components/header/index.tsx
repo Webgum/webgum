f11f7a6f7448952a0d39490ea5e941d69b7f30f5
Ijarotimi Tomilayo <moladeijarotimi@gmail.com>
2022-11-08 10:16:34 +0100

    header hamburger

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -10,12 +10,29 @@ const Header = () => {
   return (
     <div className={styles.container}>
       <div className={styles.logo}>webgum</div>
+      <div>
+        <div className={styles.menus}>
+          <button className={styles.sellBtn}>Start Selling</button>
+          <AiOutlineUser size="35px" />
+          <AiOutlineShoppingCart size="35px" />
+          <AiOutlineSearch size="35px" />
+        </div>
 
-      <div className={styles.menus}>
-        <button className={styles.sellBtn}>Start Selling</button>
-        <AiOutlineUser size="35px" />
-        <AiOutlineShoppingCart size="35px" />
-        <AiOutlineSearch size="35px" />
+        <div className={styles.hamburgerContainer}>
+          <input
+            type="checkbox"
+            id="checkbox"
+            className={`${styles.checkbox} ${styles.visuallyHidden}`}
+          />
+          <label htmlFor="checkbox">
+            <div className={styles.hamburger}>
+              <span className={`${styles.bar} ${styles.bar1}`}></span>
+              <span className={`${styles.bar} ${styles.bar2}`}></span>
+              <span className={`${styles.bar} ${styles.bar3}`}></span>
+              <span className={`${styles.bar} ${styles.bar4}`}></span>
+            </div>
+          </label>
+        </div>
       </div>
     </div>
   );
